eee4e69f0d5f81f68e732798f3d1820750af1131
Jeffermus <70596979+Jeffermus@users.noreply.github.com>
2021-10-17 17:04:36 +0200

    [2nd commit]

--- mandatory/Mandatory/app.js
+++ mandatory/Mandatory/app.js
@@ -1,16 +1,30 @@
 var express = require('express')
 var app = express()
+const path = require('path');
 const axios = require('axios').default;
 
 const hostname = 'localhost';
 const port = 8080;
 
 
+app.use(express.static(path.join(__dirname + '/public/')));
+
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
   });
 
+  app.get("/array", (req, res) => {
+    res.sendFile(__dirname + "/public/array.html");
+  });
 
+  app.get("/loops", (req, res) => {
+    res.sendFile(__dirname + "/public/loops.html");
+  });
+
+  app.get("/restful", (req, res) => {
+    res.sendFile(__dirname + "/public/restful.html");
+  });
 
 
 app.listen(8080, (error) => {
